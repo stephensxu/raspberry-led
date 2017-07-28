764a66033e28546edfb25a60d96331822944eb84
stephensxu <stephensxub@gmail.com>
2017-07-27 23:34:15 -0700

    fix filename

--- client.js
+++ client.js
@@ -1,20 +1,25 @@
 var gpio = require('rpi-gpio');
 
 var pin = 3;
+var timeToBeOn = 2000;
 
-gpio.setup(3, gpio.DIR_OUT, on);
+gpio.setup(pin, gpio.DIR_OUT, write);
 
-function on() {
-  gpio.write(3, true, function(err) {
+function write() {
+  on(pin, timeToBeOn);
+}
+
+function on(pinNumber, time) {
+  gpio.write(pinNumber, true, function(err) {
     if (err) throw err;
     console.log('Written on to pin');
   });
 
-  setTimeout(function(){ off() }, 1200);
+  setTimeout(function(){ off(pinNumber) }, time);
 }
 
-function off() {
-  gpio.write(3, false, function(err) {
+function off(pinNumber) {
+  gpio.write(pinNumber, false, function(err) {
     if (err) throw err;
     console.log("Written false to pin");
   });
